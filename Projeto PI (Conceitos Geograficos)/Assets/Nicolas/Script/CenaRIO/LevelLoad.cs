6fbe80221950f66691872191dd5b91947c427150
Anuller <nicolasrodriguesborges@gmail.com>
2023-10-14 12:27:36 -0300

    LoadScane

--- Projeto PI (Conceitos Geograficos)/Assets/Nicolas/Script/CenaRIO/LevelLoad.cs
+++ Projeto PI (Conceitos Geograficos)/Assets/Nicolas/Script/CenaRIO/LevelLoad.cs
@@ -1,11 +1,33 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoad : MonoBehaviour
 {
-    
+    public GameObject loadingScene;
+    public Slider slider;
+    public Text textProgress;
 
+    public void LoadLevel(int sceneIndex)
+    {
+       StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
 
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
+        loadingScene.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            textProgress.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
 }
